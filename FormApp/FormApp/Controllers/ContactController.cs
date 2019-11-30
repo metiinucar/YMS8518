6aa4a66e02b4d14e26484521d021215e53aa3d3a
metin uçar <metiinucar@gmail.com>
2019-11-30 19:43:47 +0300

    30.11.2019

---
+++ FormApp/FormApp/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Send([FromBody]FormApp.Data.ContactSendDto contactSendDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(error: "Hayırdır?");
+            }
+            return new JsonResult("ok");
+        }
+    }
+}
