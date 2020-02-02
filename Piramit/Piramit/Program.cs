3bf82c69339156273a05c5ef9b89be3188cc19e6
metin uçar <metiinucar@gmail.com>
2020-02-02 15:39:49 +0300

    Piramit

---
+++ Piramit/Piramit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Piramit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input1 :");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input2 :");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int ekleme = 1;
+            int[] arr = new int[x + 1];
+            for (int sayac = 1; sayac <=x; sayac++)
+            {
+                arr[sayac] = ekleme;
+                ekleme = ekleme + y;
+
+            }
+            Console.WriteLine(arr[arr.Length - 1]);
+
+            int toplam = 0;
+            for (int sayac = 1; sayac <= x; sayac++)
+                toplam = toplam + arr[sayac];
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+
+        }
+    }
+}
